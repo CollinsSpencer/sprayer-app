1cec91e65af0907bca83bf740d012373558381f9
Josh Kostal <joshkostal4@gmail.com>
2019-04-15 20:06:31 -0500

    Beautified record data page

--- client/src/containers/RecordPage.js
+++ client/src/containers/RecordPage.js
@@ -4,9 +4,9 @@ import {
   withRouter,
 } from 'react-router-dom'
 import {
-	Form,
-	FormGroup,
-	Col
+  Form,
+  Col,
+  Container
 } from 'react-bootstrap'
 import BackButton from '../components/BackButton'
 import OwnerSelector from '../components/OwnerSelector'
@@ -16,29 +16,43 @@ import SpraySelector from '../components/SpraySelector'
 class RecordPage extends Component {
   render() {
     return (
-      <Form>
-      	<FormGroup row>
-	        <Col>
-	        	<BackButton></BackButton>
-	        </Col>
-	        <Col>
-	        	<h2>Record Data</h2>
-	        </Col>
-        </FormGroup>
-      	<FormGroup row>
-	      	<Col>
-	      		<OwnerSelector></OwnerSelector>
-	      	</Col>
-	      	<Col>
-	      		<FieldSelector></FieldSelector>
-	      	</Col>
-      	</FormGroup>
-      	<FormGroup row>
-      		<Col>
-      			<SpraySelector></SpraySelector>
-      		</Col>
-      	</FormGroup>
-      </Form>
+      <Container>
+        <Form>
+        	<Form.Row>
+  	        <Col xs={5} md={4}>
+  	        	<BackButton></BackButton>
+  	        </Col>
+  	        <Col xs={8} md={4} style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+  	        	<h2>Record Data</h2>
+  	        </Col>
+            <Col xs={5} md={4}>
+            </Col>
+          </Form.Row>
+        	<Form.Row>
+            <Col>
+        		  <OwnerSelector></OwnerSelector>
+            </Col>
+            <Col>
+        		  <FieldSelector></FieldSelector>
+            </Col>
+        	</Form.Row>
+        	<Form.Row>
+            <Col md={{ span:6, offset:3 }}>
+      			 <SpraySelector></SpraySelector>
+            </Col>
+          </Form.Row>
+          <Form.Row>
+      			<Col>
+      				<Form.Label>Amount</Form.Label>
+      				<Form.Control type="number"/>
+      			</Col>
+            <Col>
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="number"/>
+            </Col>
+        	</Form.Row>
+        </Form>
+      </Container>
     )
   }
 }
