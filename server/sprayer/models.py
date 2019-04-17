e64683fa9e5eca1a6363fa394781aaa29e309934
Jared Fuelberth <jaredfuelberth@huskers.unl.edu>
2019-04-17 11:58:09 -0500

    Fix field season relation; remove dead code.

--- server/sprayer/models.py
+++ server/sprayer/models.py
@@ -21,26 +21,6 @@ class Field(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
-class SprayApplication(models.Model):
-    # COST_UNIT_CHOICES = (
-    #     (FLUID_OUNCES, 'Fluid Ounces'),
-    #     (GALLONS, 'Gallons'),
-    #     (JUNIOR, 'Junior'),
-    #     (SENIOR, 'Senior'),
-    # )
-    cost = models.IntegerField()  # Always in cents
-    # cost_unit = models.CharField()
-    amount = models.DecimalField(max_digits=8, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True, editable=False)
-    spray = models.ForeignKey(Spray, on_delete=models.PROTECT)
-    field = models.ForeignKey(Field, on_delete=models.PROTECT)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        self.updated_at = timezone.now()
-        return super(SprayApplication, self).save(*args, **kwargs)
-
-
 class FieldSeason(models.Model):
     CROP_TYPES = (
         ('CORN', 'Corn'),
@@ -52,3 +32,16 @@ class FieldSeason(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     field = models.ForeignKey(Field, on_delete=models.CASCADE)
+
+
+class SprayApplication(models.Model):
+    cost = models.IntegerField()  # In cents
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True, editable=False)
+    spray = models.ForeignKey(Spray, on_delete=models.PROTECT)
+    field_season = models.ForeignKey(FieldSeason, on_delete=models.PROTECT)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        return super(SprayApplication, self).save(*args, **kwargs)
