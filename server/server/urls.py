a05ffc26d9aae528024026fedfd69d361dd0334e
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2019-04-17 13:21:50 -0500

    All api endpoints start with api/

--- server/server/urls.py
+++ server/server/urls.py
@@ -29,6 +29,6 @@ router.register(r'field-seasons', views.FieldSeasonViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
