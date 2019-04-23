3ffdc8a989e3002ec43f9aafea0fa37e3f06f53e
Josh Kostal <joshkostal4@gmail.com>
2019-04-23 10:51:47 -0500

    Merged and fixed errors

--- client/src/components/SprayBar.js
+++ client/src/components/SprayBar.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 import {
-  Link,
   withRouter
 } from 'react-router-dom'
+import { connect } from 'react-redux'
 import {
   Navbar,
-  Nav
+  Nav,
+  NavDropdown
 } from 'react-bootstrap'
-<<<<<<< HEAD
 
-class SprayBar extends Component {
-  handleClick(e) {
-    e.preventDefault()
-=======
 import { LinkContainer } from 'react-router-bootstrap'
 import {
   Modes,
@@ -32,7 +28,6 @@ class SprayBar extends Component {
       return 'Harvesting'
     }
     return 'Select Mode'
->>>>>>> cd22fc9e71a6b4fd5c64fd4d13c7876a100d2ca5
   }
 
   render() {
@@ -43,10 +38,6 @@ class SprayBar extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-<<<<<<< HEAD
-            <Nav.Link href="/record" onSelect={(event) => this.handleClick(event)}>Record Data</Nav.Link>
-            <Nav.Link href="/data" onSelect={(event) => this.handleClick(event)}>View Data</Nav.Link>
-=======
             <LinkContainer to="/record"><Nav.Link>Record Data</Nav.Link></LinkContainer>
             <LinkContainer to="/data"><Nav.Link>View Data</Nav.Link></LinkContainer>
             <NavDropdown title={this.getModeDropdownText()} id="basic-nav-dropdown">
@@ -54,7 +45,6 @@ class SprayBar extends Component {
               <NavDropdown.Item active={this.props.currentMode === Modes.SPRAYING} onSelect={this.props.setModeToSpraying}>Spray</NavDropdown.Item>
               <NavDropdown.Item active={this.props.currentMode === Modes.HARVESTING} onSelect={this.props.setModeToHarvesting}>Harvest</NavDropdown.Item>
             </NavDropdown>
->>>>>>> cd22fc9e71a6b4fd5c64fd4d13c7876a100d2ca5
           </Nav>
           { auth.isAuthenticated ? (
             <LogoutButton></LogoutButton>
@@ -67,9 +57,6 @@ class SprayBar extends Component {
   }
 }
 
-<<<<<<< HEAD
-export default withRouter(SprayBar)
-=======
 const mapStateToProps = (state) => {
   const { auth, mode } = state
   return {
@@ -85,4 +72,3 @@ const mapDispatchToProps = {
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SprayBar))
->>>>>>> cd22fc9e71a6b4fd5c64fd4d13c7876a100d2ca5
--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -9,16 +9,12 @@ import {
 } from 'react-bootstrap'
 import SprayBar from '../components/SprayBar'
 import RecentData from '../components/RecentData'
-import ModeSelector from '../components/ModeSelector'
 
 class HomePage extends Component {
   render() {
     return (
       <Container>
         <SprayBar></SprayBar>
-        <Row>
-          <ModeSelector></ModeSelector>
-        </Row>
         <Row style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
           <RecentData></RecentData>
         </Row>
